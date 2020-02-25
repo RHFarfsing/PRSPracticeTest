636075897fe974f36277468f7d4cd8e0ed7ad5c5
Robert Farfsing <robertfarfsing@gmail.com>
2020-02-25 16:32:50 -0500

    migrating things

---
+++ PRSLibrary/Migrations/20200225213049_redid clss and model.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PRSLibrary.Migrations
+{
+    public partial class redidclssandmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(maxLength: 30, nullable: false),
+                    Password = table.Column<string>(maxLength: 30, nullable: false),
+                    Firstname = table.Column<string>(maxLength: 30, nullable: false),
+                    Lastname = table.Column<string>(maxLength: 30, nullable: false),
+                    Phone = table.Column<string>(maxLength: 12, nullable: true),
+                    Email = table.Column<string>(maxLength: 255, nullable: true),
+                    IsReviewer = table.Column<bool>(nullable: false, defaultValue: false),
+                    IsAdmin = table.Column<bool>(nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(maxLength: 30, nullable: false),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Address = table.Column<string>(maxLength: 30, nullable: false),
+                    City = table.Column<string>(maxLength: 30, nullable: false),
+                    State = table.Column<string>(maxLength: 2, nullable: false),
+                    Zip = table.Column<string>(maxLength: 5, nullable: false),
+                    Phone = table.Column<string>(maxLength: 12, nullable: true),
+                    Email = table.Column<string>(maxLength: 225, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartNbr = table.Column<string>(maxLength: 30, nullable: false),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(11,2)", nullable: false),
+                    Unit = table.Column<string>(maxLength: 30, nullable: false),
+                    VendorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Vendors_VendorId",
+                        column: x => x.VendorId,
+                        principalTable: "Vendors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_VendorId",
+                table: "Products",
+                column: "VendorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendors_Code",
+                table: "Vendors",
+                column: "Code",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Vendors");
+        }
+    }
+}
