9130f0e58ea209c859c019ec785d7cbdda5beadc
Robert Farfsing <robertfarfsing@gmail.com>
2020-02-25 14:24:41 -0500

    Add project files.

---
+++ PRSLibrary/PRSDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRSLibrary {
+    public class PRSDbContext {
+        //public virtual DbSet<User> Users { get; set; }
+        //public PRSDbContext() { }
+        //public PRSDbContext(DbContextOptions<PRSDbContext>options): base(options) { }
+        //protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+        //    if (!builder.IsConfigured) {
+        //        var connStr = @"server=localhost\sqlsepress;database="
+        //    }
+        //}
+    }
+}
---
+++ PRSPracticeTest/Program.cs
@@ -0,0 +1,10 @@
+﻿using PRSLibrary;
+using System;
+
+namespace PRSPracticeTest {
+    class Program {
+        static void Main(string[] args) {
+            var context = new PRSDbContext();
+        }
+    }
+}
